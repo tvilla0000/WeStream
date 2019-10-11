d1d83bea8879c01f12032b87f2097b9a02782cc7
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-11 16:57:38 -0700

    add middleware for routing

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, "build")));
 
 // Put API routes here, before the "catch all" route
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/videos", require("./routes/api/videos"));
 
 // The following "catch all" route (note the *)is necessary
 // for a SPA's client-side routing to properly work
