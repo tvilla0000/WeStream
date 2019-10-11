e35da4d050430db9575116d6f02427e566b54b16
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-11 16:59:34 -0700

    import and render components in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Main Components/NavBar";
 import { Route, Switch } from "react-router-dom";
 import LoginPage from "./components/Auth/LoginPage";
 import SignupPage from "./components/Auth/SignupPage";
+import userService from "./utils/userService";
+import youtube from "./APIs/youtube";
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Route
-        exact
-        path="/signup"
-        render={({ history }) => <SignupPage history={history} />}
-      />
-      <Route exact path="/login" render={() => <LoginPage />} />
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: userService.getUser(),
+    videos: [],
+    selVideo: null
+  };
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() });
+  };
+
+  handleSubmit = async searchBarTerm => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: searchBarTerm
+      }
+    });
+    this.setState({
+      videos: response.data.items
+    });
+  };
+
+  handleSelVideo = video => {
+    this.setState({ selVideo: video });
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar user={this.state.user} handleFormSubmit={this.handleSubmit} />
+        <Route
+          exact
+          path="/signup"
+          render={({ history }) => (
+            <SignupPage history={history} signUp={this.handleSignupOrLogin} />
+          )}
+        />
+        <Route
+          exact
+          path="/login"
+          render={({ history }) => (
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
