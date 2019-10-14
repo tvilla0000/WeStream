d4f0f70fd8b6d18d54639c258a5ffb49a86f429a
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-13 21:04:43 -0700

    create and add functionality as a video service file

---
+++ src/utils/videoService.js
@@ -0,0 +1,10 @@
+import tokenService from "./tokenService";
+
+async function searchYoutube(query) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+}
