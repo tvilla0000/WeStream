c023d8297aa8e2c24e9d0ce95b1984453fd0a9f5
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-13 21:17:16 -0700

    finish video service functionality

--- src/utils/videoService.js
+++ src/utils/videoService.js
@@ -5,7 +5,38 @@ async function searchYoutube(query) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      'Autherization'
+      Authorization: "Bearer" + tokenService.getToken()
+    },
+    body: JSON.stringify({ query: query })
+  };
+  return await fetch(`/api/videos/search`, options).then(res => res.json());
+}
+
+async function getVideo(videoId) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer" + tokenService.getToken()
+    },
+    body: JSON.stringify({ videoId: videoId })
+  };
+  return await fetch(`/api/videos/video`, options).then(res => res.json());
+}
+
+async function getVidList() {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer" + tokenService.getToken()
     }
   };
+  return fetch(`/api/videos/`, options).then(res => res.json());
 }
+
+export default {
+  searchYoutube,
+  getVideo,
+  getVidlist
+};
