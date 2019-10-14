10d96d5a3ca8a37020ad2686e3acce26f4f588fe
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-13 20:58:03 -0700

    update video model

--- controllers/videos.js
+++ controllers/videos.js
@@ -1,11 +1,31 @@
-const BASE_URL = process.env.BASE_URL;
+const YOUTUBE_URL = process.env.YOUTUBE_URL;
+const key = process.env.YOUTUBE_API_KEY;
 const fetch = require("node-fetch");
 
 module.exports = {
-  grabData
+  searchVideos,
+  getSingleVideo,
+  getVideos
 };
 
-async function grabData(req, res) {
-  const url = `${BASE_URL}search?part=snippet&q=${req.body.query}&key=${process.env.YOUTUBE_API_KEY}`;
+// This Should Pull one Video in JSON Format from Youtube API!!
+async function searchVideos(req, res) {
+  const url = `${YOUTUBE_URL}search?part=snippet&q=${req.body.query}&key=${key}`;
   return res.json(await fetch(url).then(res => res.json()));
+}
+
+async function getVideos(req, res) {
+  let videos = await Video.find({});
+  const mapVids = videos.map(video =>
+    fetch(
+      `${YOUTUBE_URL}videos?part=snippet&id=${video.videoId}&key=${key}`
+    ).then(res => res.json())
+  );
+  const vidList = await Promise.all(mapVids);
+  res.json(vidList);
+}
+
+async function getSingleVideo(req, res) {
+  const url = `${YOUTUBE_URL}videos?part=snippet&id=${req.body.videoId}&key=${key}`;
+  return res.json(await fetch(url).then(res => res.json));
 }
--- models/video.js
+++ models/video.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const videoSchema = new Schema({
-  userId: String,
-  videoId: String,
-  watchLater: false
-});
+const videoSchema = new Schema(
+  {
+    userId: String,
+    videoId: String,
+    watchLater: false
+  },
+  {
+    timesStamps: true
+  }
+);
 
 module.exports = mongoose.model("Video", videoSchema);
