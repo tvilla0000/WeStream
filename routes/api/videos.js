1d0d6f750889d98b8b357ab3fd3afafde88e0780
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-13 21:00:50 -0700

    create and implement backend functionality for videos

--- routes/api/videos.js
+++ routes/api/videos.js
@@ -2,6 +2,8 @@ const express = require("express");
 const videoCtrl = require("../../controllers/videos");
 const router = express.Router();
 
-router.post("/", videoCtrl.grabData);
+router.get("/", videoCtrl.getVideos);
+router.post("/video", videoCtrl.getSingleVideo);
+router.post("/search", videoCtrl.searchVideos);
 
 module.exports = router;
