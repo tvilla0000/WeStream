d6338262559a090af0da47adeffb31fff00f6008
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-15 13:49:08 -0700

    create Pages Folder

---
+++ src/Pages/VideoView.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Video from "../components/MainComponents/Video";
+class VideoView extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Video />
+      </div>
+    );
+  }
+}
+
+export default VideoView;
